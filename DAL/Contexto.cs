67ee222baa3c9db2c87e46d3044a45ff83582a07
Fabio cortorreal <Fabiocortorreal218@gmail.com>
2026-02-02 19:44:55 -0400

    Crea Contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,11 @@
+﻿using Luis_CortorrealAP1_P1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Luis_CortorrealAP1_P1.DAL;
+
+public class Contexto : DbContext
+{
+    public Contexto(DbContextOptions options) : base(options) { }
+    public DbSet<ViajesEspaciales> ViajesEspaciales { get; set; }
+}
+
