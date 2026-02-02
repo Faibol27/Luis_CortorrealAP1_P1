e6ed94a6b281b55dc30ebb8cd9f9ee8e8c828296
Fabio cortorreal <Fabiocortorreal218@gmail.com>
2026-02-02 19:35:41 -0400

    Crea modelo con sus parametros

---
+++ Models/ViajesEspaciales.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Luis_CortorrealAP1_P1.Models;
+
+    public class ViajesEspaciales
+    {
+        [Key]
+        public int ViajeId { get; set; }
+
+        [Required] 
+        public String Descripcion { get; set; }
+
+        [Required] 
+        public DateTime Fecha { get; set; }
+
+        [Required] 
+        public double Precio { get; set; }
+    }
+
